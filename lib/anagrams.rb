5ba385cb5f9a7e5dc26dd2e66b14dbfe2dc930da
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-02 18:49:45 +0000

    Anagram Done

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,2 +1,26 @@
 def anagrams?(str1, str2)
+    array1 = str1.split("")
+    hash1 = Hash.new
+    array1.each do |a|
+        occurances = 0
+        array1.each do |b|
+            if a == b
+                occurances += 1
+            end
+        end
+        hash1[a] = occurances
+    end
+    
+    array2 = str2.split("")
+    hash2 = Hash.new
+    array2.each do |a|
+        occurances = 0
+        array2.each do |b|
+            if a == b
+                occurances += 1
+            end
+        end
+        hash2[a] = occurances
+    end
+    hash1 == hash2
 end
